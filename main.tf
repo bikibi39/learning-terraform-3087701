d7656ad343dc51fdbefe7bbceafcf33569e983c8
bikibi39 <bikibi39@rowan.edu>
2025-05-05 23:00:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,28 +18,44 @@ data "aws_vpc" "default" {
    default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "HelloWorld"
   }
 }
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name = "blog_new"
+  source               = "terraform-aws-modules/security-group/aws"
+  version              = "5.3.0"
+  name                 = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
+
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in. Allow everything out"
